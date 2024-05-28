3d2a0cb0d28db8e2a67ec2678a59e6c264227f36
mert-celik-1 <mertcelik2399@gmail.com>
2024-05-28 18:53:26 +0300

    ocelot-gateway-auth

--- 01-ApiGateway/ApiGateway-API1/Program.cs
+++ 01-ApiGateway/ApiGateway-API1/Program.cs
@@ -1,19 +1,12 @@
-// api 1 = https://localhost:7143
-// api 2 = https://localhost:7055
-// api 3 = https://localhost:7299
-
-// gateway = http://localhost:5273
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json");
-builder.Services.AddOcelot(builder.Configuration);
 
+builder.Services.AddAuthorization(options =>
+    options.AddPolicy("AdminPolicy", policy => policy.RequireClaim("Role", "Admin")));
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
     {
@@ -25,17 +18,18 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuerSigningKey = true,
             ValidIssuer = builder.Configuration["Token:Issuer"],
             ValidAudience = builder.Configuration["Token:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
+            IssuerSigningKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
             ClockSkew = TimeSpan.Zero
         };
     });
 
 var app = builder.Build();
-
+ 
 app.UseAuthentication();
 app.UseAuthorization();
 
-await app.UseOcelot();
-app.UseHttpsRedirection();
+app.MapGet("/", () => "API 1")
+    .RequireAuthorization("AdminPolicy");
 
 app.Run();
